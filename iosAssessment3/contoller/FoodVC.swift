8950103a6faaf2fa5eaa8f91d0b96bf458cad94f
Harry722 <83649246+Harry722@users.noreply.github.com>
2022-05-15 23:44:45 +1000

    Add food graph and calories

--- iosAssessment3/contoller/FoodVC.swift
+++ iosAssessment3/contoller/FoodVC.swift
@@ -13,16 +13,28 @@ class FoodVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        foodList.append(Food(foodname:"1号",calories: 100,img: UIImage(named:"1.jpg")!))
-        foodList.append(Food(foodname:"2号",calories: 100,img: UIImage(named:"2.jpg")!))
-        foodList.append(Food(foodname:"3号",calories: 100,img: UIImage(named:"3.jpg")!))
-        foodList.append(Food(foodname:"4号",calories: 100,img: UIImage(named:"4.jpg")!))
-        foodList.append(Food(foodname:"5号",calories: 100,img: UIImage(named:"5.jpg")!))
-        foodList.append(Food(foodname:"6号",calories: 100,img: UIImage(named:"6.jpg")!))
-        foodList.append(Food(foodname:"7号",calories: 100,img: UIImage(named:"7.jpg")!))
-        foodList.append(Food(foodname:"8号",calories: 100,img: UIImage(named:"8.jpg")!))
-        foodList.append(Food(foodname:"9号",calories: 100,img: UIImage(named:"9.jpg")!))
-        foodList.append(Food(foodname:"10号",calories: 100,img: UIImage(named:"10.jpg")!))
+        foodList.append(Food(foodname:"Egg",calories: 167,img: UIImage(named:"egg.jpg")!))
+        foodList.append(Food(foodname:"Chicken breast",calories: 133,img: UIImage(named:"chicken.jpg")!))
+        foodList.append(Food(foodname:"beef",calories: 106,img: UIImage(named:"beef.jpg")!))
+        foodList.append(Food(foodname:"Pork",calories: 395,img: UIImage(named:"pork.jpg")!))
+        foodList.append(Food(foodname:"fish",calories: 113,img: UIImage(named:"fish.jpg")!))
+        foodList.append(Food(foodname:"Shrimp",calories: 85,img: UIImage(named:"shrimp.jpg")!))
+        foodList.append(Food(foodname:"Lamb",calories: 217,img: UIImage(named:"lamb.jpg")!))
+        foodList.append(Food(foodname:"corn",calories: 71,img: UIImage(named:"corn.jpg")!))
+        foodList.append(Food(foodname:"Pumpkin",calories: 74,img: UIImage(named:"pumpkin.jpg")!))
+        foodList.append(Food(foodname:"Seaweed",calories: 16,img: UIImage(named:"seaweed.jpg")!))
+        foodList.append(Food(foodname:"Mushroom",calories: 27,img: UIImage(named:"mushroom.jpg")!))
+        foodList.append(Food(foodname:"Orange",calories: 43,img: UIImage(named:"orange.jpg")!))
+        foodList.append(Food(foodname:"Grape",calories: 33,img: UIImage(named:"grape.jpg")!))
+        foodList.append(Food(foodname:"Mango",calories: 36,img: UIImage(named:"mango.jpg")!))
+        foodList.append(Food(foodname:"Apple",calories: 46,img: UIImage(named:"apple.jpg")!))
+        foodList.append(Food(foodname:"Watermelon",calories: 25,img: UIImage(named:"watermelon.jpg")!))
+        foodList.append(Food(foodname:"Peach",calories: 43,img: UIImage(named:"peach.jpg")!))
+        foodList.append(Food(foodname:"Chips",calories: 536,img: UIImage(named:"chips.jpg")!))
+        foodList.append(Food(foodname:"Cheese",calories: 349,img: UIImage(named:"cheese.jpg")!))
+        foodList.append(Food(foodname:"Bread",calories: 265,img: UIImage(named:"bread.jpg")!))
+        foodList.append(Food(foodname:"Pasta",calories: 131,img: UIImage(named:"pasta.jpg")!))
+       
     }
 }
 
